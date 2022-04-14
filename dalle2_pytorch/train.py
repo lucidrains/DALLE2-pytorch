ffaf1149adeb1e4c56aa2611b6ee003b563133ea
Aniket Maurya <theaniketmaurya@gmail.com>
2022-04-15 02:26:07 +0530

    scratch trainer

--- dalle2_pytorch/train.py
+++ dalle2_pytorch/train.py
@@ -1,43 +1,40 @@
 import torch
+import torch.utils.data
+from torch import nn
+
 from dalle2_pytorch import CLIP
 
-import pytorch_lightning as pl
-import torch.utils.data
-from pytorch_lightning.utilities.cli import LightningCLI
-from torch import nn
-from x_clip import CLIP
 
-
-class CLIPModule(pl.LightningModule):
+class CLIPTrainer:
     def __init__(
-            self,
-            image_encoder: nn.Module = None,
-            text_encoder: nn.Module = None,
-            dim_text=512,
-            dim_image=512,
-            dim_latent=512,
-            num_text_tokens=10000,
-            text_enc_depth=6,
-            text_seq_len=256,
-            text_heads=8,
-            visual_enc_depth=6,
-            visual_image_size=256,
-            visual_patch_size=32,
-            visual_heads=8,
-            use_all_token_embeds=True,
-            decoupled_contrastive_learning=True,
-            extra_latent_projection=True,
-            use_visual_ssl=True,
-            visual_ssl_type="simclr",
-            use_mlm=False,
-            text_ssl_loss_weight=0.05,
-            image_ssl_loss_weight=0.05,
-            freeze_image_encoder: bool = False,
+        self,
+        image_encoder: nn.Module = None,
+        text_encoder: nn.Module = None,
+        dim_text=512,
+        dim_image=512,
+        dim_latent=512,
+        num_text_tokens=10000,
+        text_enc_depth=6,
+        text_seq_len=256,
+        text_heads=8,
+        visual_enc_depth=6,
+        visual_image_size=256,
+        visual_patch_size=32,
+        visual_heads=8,
+        use_all_token_embeds=True,
+        decoupled_contrastive_learning=True,
+        extra_latent_projection=True,
+        use_visual_ssl=True,
+        visual_ssl_type="simclr",
+        use_mlm=False,
+        text_ssl_loss_weight=0.05,
+        image_ssl_loss_weight=0.05,
+        freeze_image_encoder: bool = False,
+        log_interval=10,
     ):
         super().__init__()
-        self.automatic_optimization = False
+        self.log_interval = log_interval
         self.freeze_image_encoder = freeze_image_encoder
-        self.save_hyperparameters()
         self.clip = CLIP(
             image_encoder=image_encoder,
             text_encoder=text_encoder,
@@ -62,7 +59,7 @@ class CLIPModule(pl.LightningModule):
             image_ssl_loss_weight=image_ssl_loss_weight,
         )
 
-    def forward(self, texts, images):
+    def shared_step(self, texts, images):
         loss = self.clip(
             texts,
             images,
@@ -71,35 +68,52 @@ class CLIPModule(pl.LightningModule):
         )
         return loss
 
-    def training_step(self, batch):
-        texts, images = batch
-        loss = self(texts, images)
-        self.log("train/loss", loss)
+    @property
+    def device(self):
+        return "cuda" if torch.cuda.is_available() else "cpu"
 
-    def validation_step(self, batch):
-        texts, images = batch
-        loss = self(texts, images)
-        self.log("val/loss", loss)
+    def train_epoch(self, epoch: int, train_dataloader):
+        self.clip.train()
+        for batch_idx, (texts, images) in enumerate(train_dataloader):
+            texts, images = texts.to(self.device), images.to(self.device)
+            loss = self.shared_step(texts, images)
+            if (batch_idx == 0) or ((batch_idx + 1) % self.log_interval == 0):
+                print(
+                    "Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}".format(
+                        epoch,
+                        batch_idx * len(images),
+                        len(train_dataloader.dataset),
+                        100.0 * batch_idx / len(train_dataloader),
+                        loss.item(),
+                    )
+                )
 
-    def configure_optimizers(self):
-        """using manual optimization"""
+    def val_epoch(self, epoch: int, val_dataloader):
+        if val_dataloader is None:
+            return
+        self.clip.eval()
+        test_loss = 0
+        with torch.no_grad():
+            for texts, images in val_dataloader:
+                texts, images = texts.to(self.device), images.to(self.device)
+                test_loss = self.shared_step(texts, images)
 
+        test_loss /= len(val_dataloader.dataset)
 
-def cli_main(
-        datamodule=None, train_dataloader=None, val_dataloader=None, test_dataloader=None
-):
-    cli = LightningCLI(CLIPModule, save_config_overwrite=True, run=False)
-    cli.trainer.fit(
-        cli.model,
-        datamodule=datamodule,
-        train_dataloaders=train_dataloader,
-        val_dataloaders=val_dataloader,
-    )
-    if test_dataloader:
-        cli.trainer.test(ckpt_path="best", test_dataloader=test_dataloader)
+        print(
+            "\nTest Epoch: Average loss: {:.4f}\n".format(
+                epoch, test_loss, len(val_dataloader.dataset)
+            )
+        )
+
+    def run(self, max_epochs: int, train_dataloader, val_dataloader=None):
+        for epoch in range(1, max_epochs + 1):
+            self.train_epoch(epoch, train_dataloader=train_dataloader)
+            self.val_epoch(epoch, val_dataloader=val_dataloader)
 
 
 if __name__ == "__main__":
+
     class MockDataset(torch.utils.data.Dataset):
         def __init__(self):
             super(MockDataset, self).__init__()
@@ -112,6 +126,6 @@ if __name__ == "__main__":
         def __len__(self):
             return 10
 
-
     dataloader = torch.utils.data.DataLoader(MockDataset(), batch_size=4)
-    cli_main(train_dataloader=dataloader)
+    clip_trainer = CLIPTrainer()
+    clip_trainer.run(1, dataloader)
