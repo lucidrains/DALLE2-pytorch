ddde8ca1bf3d26e3274b5b452a480271a1446e34
Phil Wang <lucidrains@gmail.com>
2022-04-19 20:54:28 -0700

    fix cosine  bbeta schedule, thanks to @Zhengxinyang

--- dalle2_pytorch/dalle2_pytorch.py
+++ dalle2_pytorch/dalle2_pytorch.py
@@ -105,8 +105,8 @@ def cosine_beta_schedule(timesteps, s = 0.008):
     as proposed in https://openreview.net/forum?id=-NEXDKk8gZ
     """
     steps = timesteps + 1
-    x = torch.linspace(0, steps, steps)
-    alphas_cumprod = torch.cos(((x / steps) + s) / (1 + s) * torch.pi * 0.5) ** 2
+    x = torch.linspace(0, timesteps, steps)
+    alphas_cumprod = torch.cos(((x / timesteps) + s) / (1 + s) * torch.pi * 0.5) ** 2
     alphas_cumprod = alphas_cumprod / alphas_cumprod[0]
     betas = 1 - (alphas_cumprod[1:] / alphas_cumprod[:-1])
     return torch.clip(betas, 0, 0.999)
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
       'dream = dalle2_pytorch.cli:dream'
     ],
   },
-  version = '0.0.25',
+  version = '0.0.26',
   license='MIT',
   description = 'DALL-E 2',
   author = 'Phil Wang',
