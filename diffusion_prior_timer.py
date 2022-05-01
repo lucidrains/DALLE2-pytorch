dba193240c97c41e6ecdb19a8d0a3eb2917e830c
Kumar R <kumar.ramanathan@gmail.com>
2022-05-01 21:54:10 +0530

    Timer for handling async save of checkpoints.

---
+++ diffusion_prior_timer.py
@@ -0,0 +1,39 @@
+import threading 
+import datetime
+import time
+import torch
+from time import sleep
+
+class RepeatedTimer(object):
+  def __init__(self, interval, function, *args, **kwargs):
+    self._timer = None
+    self.interval = interval
+    self.function = function
+    self.args = args
+    self.kwargs = kwargs
+    self.is_running = False
+    self.next_call = time.time()
+    self.start()
+
+  def _run(self):
+    self.is_running = False
+    self.start()
+    self.function(*self.args, **self.kwargs)
+
+  def start(self):
+    if not self.is_running:
+      self.next_call += self.interval
+      self._timer = threading.Timer(self.next_call - time.time(), self._run)
+      self._timer.start()
+      self.is_running = True
+
+  def stop(self):
+    self._timer.cancel()
+    self.is_running = False
+
+
+
+def save_model(save_path,state_dict):
+    # Saving State Dict
+    print("====================================== Saving checkpoint ======================================")
+    torch.save(state_dict, save_path+'/'+str(time.time())+'saved_model.pth')
