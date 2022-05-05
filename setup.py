d8d8b6caf1a38df3b43b565a590c8454650f1103
Phil Wang <lucidrains@gmail.com>
2022-05-05 07:09:45 -0700

    dataloaders for decoder training, from @Veldrovive

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
       'dream = dalle2_pytorch.cli:dream'
     ],
   },
-  version = '0.0.101',
+  version = '0.0.102',
   license='MIT',
   description = 'DALL-E 2',
   author = 'Phil Wang',
