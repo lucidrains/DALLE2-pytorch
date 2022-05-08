6bd7ae1105608dfda309fe63897e63cf748dbcf9
Kumar R <kumar.ramanathan@gmail.com>
2022-05-07 18:31:41 -0700

    Update __init__.py

--- dalle2_pytorch/__init__.py
+++ dalle2_pytorch/__init__.py
@@ -1,4 +1,4 @@
-from dalle2_pytorch.dalle2_pytorch import DALLE2, DiffusionPriorNetwork, DiffusionPrior, Unet, Decoder, load_diffusion_prior
+from dalle2_pytorch.dalle2_pytorch import DALLE2, DiffusionPriorNetwork, DiffusionPrior, Unet, Decoder, load_diffusion_model, save_diffusion_model
 from dalle2_pytorch.dalle2_pytorch import OpenAIClipAdapter
 from dalle2_pytorch.train import DecoderTrainer, DiffusionPriorTrainer
 from dalle2_pytorch.train_diffusion_prior import load_diffusion_model, save_diffusion_model
